a9ab124224ebf9cb941ff868285439a325c86914
wisatec <31212843+wisatec@users.noreply.github.com>
2018-04-16 16:45:06 -0300

    subindo

--- Constantes.php
+++ Constantes.php
@@ -3,6 +3,6 @@
 define('HOST','mysql:host=localhost;dbname=grupowis_bd_ERP');
 define('USER', 'grupowis_well');
 define('PASS', 'jwmiamm130687');
-define('OPT',array(PDO::ATTR_PERSISTENT => true,PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES latin1',));
+define('OPT',array(PDO::ATTR_PERSISTENT => true,PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',));
 
 ?>
--- Fornecedores.php
+++ Fornecedores.php
@@ -9,7 +9,7 @@
         }
 		function PesquisaCnpj($value) {
 			$sql = "SELECT f.Cnpj FROM Fornecedor f WHERE f.Cnpj LIKE '".$value."'";
-			$result = 	self::sqlSelectCount($Sql);
+			$result = self::sqlSelectCount($sql);
 			return $result;
 		}	
     }
