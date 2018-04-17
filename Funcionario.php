c9de962972befddb2a379a8ae58c5cbb250c54ee
Well Menezes <grupowisatec@gmail.com>
2018-04-17 14:52:58 -0300

    funcionario idade

---
+++ Funcionario.php
@@ -0,0 +1,20 @@
+<?php
+   /**
+    * 
+    */
+   class Funcionario extends Conexao {
+       
+       function __construct($argument) {
+           
+       }
+	   
+	   function Idade($nova_data){
+	   	
+			$sql = "SELECT CONCAT(TIMESTAMPDIFF(YEAR,". $nova_data.", NOW()),' anos') AS idade";
+			$result = self::sqlSelectOne($sql);
+			return $result;			
+	   }
+   }
+   
+    
+?>
