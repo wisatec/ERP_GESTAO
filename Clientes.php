cc3ae582ae4ca50a4b5ae3296c3123f691ab559e
wisatec <31212843+wisatec@users.noreply.github.com>
2018-04-17 18:55:09 -0300

    Função ExisteCpf

--- Clientes.php
+++ Clientes.php
@@ -15,6 +15,12 @@
 			$result = self::sqlRecordBit($sql);
 			return $result;
 		}
+		function ExisteCPFUpdate($cnpj,$codigo){
+			
+			$sql = "SELECT *FROM Cliente c WHERE c.CnpjCpf = '".$cnpj."' AND c.idCliente != ".$codigo;
+			$result = self::sqlRecordBit($sql);
+			return $result;
+		}
 	}
     
 ?>
