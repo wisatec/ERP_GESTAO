506218e11a1bb2afe575b7e1c6a8b2aafcc15a29
wisatec <31212843+wisatec@users.noreply.github.com>
2018-04-18 12:11:22 -0300

    acerto

--- HistoricoFuncionario.php
+++ HistoricoFuncionario.php
@@ -10,20 +10,8 @@
 		}
 		
 		function UltimoSalario(){
-<<<<<<< HEAD
 			$result = HistoricoFuncionarioDao::UltimoSalarioDAO();
 			return $result;										
-=======
-			
-			$sql = "SELECT   CASE  
-      					WHEN hf.SalarioAtual > 0 AND hf.SalarioReaj = 0 THEN  hf.SalarioAtual
-      					WHEN hf.SalarioAtual > 0 AND hf.SalarioReaj > 0 THEN  hf.SalarioReaj
-      					END AS atual
-    					FROM HistoricoFunc hf 
-						WHERE hf.IdHist = (SELECT MAX(IdHist) from HistoricoFunc)";
-			$result = self::sqlSelectOne($sql);
-			return $result;	
->>>>>>> 527a501ed0e8064d2e36699114719431803fe929
 		}
 		
 		function CalculaPorcSal($salario,$valor){
