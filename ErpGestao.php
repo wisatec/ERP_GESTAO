6c2dfff60fdfe8ccb191f6496785a3aa9d2853e4
wisatec <31212843+wisatec@users.noreply.github.com>
2018-04-18 15:17:42 -0300

    commit

--- ErpGestao.php
+++ ErpGestao.php
@@ -1,17 +1,17 @@
 <?php
 	require_once(dirname(__FILE__).'/Constantes.php');
 	require_once(dirname(__FILE__).'/Css.php');
-	require_once(dirname(__FILE__).'MODEL/boleto.php');
-	require_once(dirname(__FILE__).'MODEL/Clientes.php');
-	require_once(dirname(__FILE__).'MODEL/Empresa.php');
-	require_once(dirname(__FILE__).'MODEL/Fornecedores.php');
-	require_once(dirname(__FILE__).'MODEL/Funcionario.php');
-	require_once(dirname(__FILE__).'MODEL/HistoricoFuncionario.php');
-	require_once(dirname(__FILE__).'DAO/Conexao.php');
-	require_once(dirname(__FILE__).'DAO/ClientesDAO.php');	
-	require_once(dirname(__FILE__).'DAO/FornecedoresDAO.php');
-	require_once(dirname(__FILE__).'DAO/FuncionarioDAO.php');
-	require_once(dirname(__FILE__).'DAO/HistoricoFuncionarioDAO.php');
+	require_once(dirname(__FILE__).'/MODEL/boleto.php');
+	require_once(dirname(__FILE__).'/MODEL/Clientes.php');
+	require_once(dirname(__FILE__).'/MODEL/Empresa.php');
+	require_once(dirname(__FILE__).'/MODEL/Fornecedores.php');
+	require_once(dirname(__FILE__).'/MODEL/Funcionario.php');
+	require_once(dirname(__FILE__).'/MODEL/HistoricoFuncionario.php');
+	require_once(dirname(__FILE__).'/DAO/Conexao.php');
+	require_once(dirname(__FILE__).'/DAO/ClientesDAO.php');	
+	require_once(dirname(__FILE__).'/DAO/FornecedoresDAO.php');
+	require_once(dirname(__FILE__).'/DAO/FuncionarioDAO.php');
+	require_once(dirname(__FILE__).'/DAO/HistoricoFuncionarioDAO.php');
 	
     /**
      * Classe Utilitários 
