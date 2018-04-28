1bfedbe8e463ca5adea7f74551826fc800d420c6
wisatec <31212843+wisatec@users.noreply.github.com>
2018-04-28 18:21:22 -0300

    commit

--- DAO/ItemDAO.php
+++ DAO/ItemDAO.php
@@ -72,7 +72,7 @@
 			return $result;
 		}
     	static function InserirCodEan($codProd,$codBarras){
-    		$sql = "INSERT INTO ProdutoCodEan(CodEan,IdProduto)VALUES('".$codBarras."',".$codProd.")";
+    		$sql = "INSERT INTO ItemCodEan(CodEan,IdProduto)VALUES('".$codBarras."',".$codProd.")";
 			$result  = self::sqlExec($sql);
 			self::close();
 			return $result;
